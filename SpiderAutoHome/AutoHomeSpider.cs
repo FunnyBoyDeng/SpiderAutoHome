92a19b41f33df17028039a3364c7ad930ff60068
FunnyBoyDeng <472646405@qq.com>
2026-09-17 23:30:48 +0800

    refactor: migrate AutoHome POST request generation

--- SpiderAutoHome/AutoHomeSpider.cs
+++ SpiderAutoHome/AutoHomeSpider.cs
@@ -1,7 +1,10 @@
+using System.Collections.Generic;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using DotnetSpider;
 using DotnetSpider.DataFlow;
+using DotnetSpider.Http;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
@@ -17,13 +20,31 @@ namespace SpiderAutoHome
         {
         }
 
-        protected override Task InitializeAsync(
+        protected override async Task InitializeAsync(
             CancellationToken stoppingToken = default)
         {
             AddDataFlow<AutoHomeParser>();
             AddDataFlow<ConsoleStorage>();
 
-            return Task.CompletedTask;
+            var requests = new List<Request>();
+
+            for (var page = 1; page <= 33; page++)
+            {
+                var formData =
+                    $"id=7&j=%7B%22createMan%22%3A%2218273159100%22%2C%22createTime%22%3A1518433690000%2C%22row%22%3A5%2C%22siteUserActivityListId%22%3A8553%2C%22siteUserPageRowModuleId%22%3A84959%2C%22topids%22%3A%22%22%2C%22wherePhase%22%3A%221%22%2C%22wherePreferential%22%3A%220%22%2C%22whereUsertype%22%3A%220%22%7D&page={page}&shopid=83106681";
+
+                var request = new Request(
+                    "https://store.mall.autohome.com.cn/shop/ajaxsitemodlecontext.jtml")
+                {
+                    Method = "POST",
+                    Content = new ByteArrayContent(
+                        Encoding.UTF8.GetBytes(formData))
+                };
+
+                requests.Add(request);
+            }
+
+            await AddRequestsAsync(requests);
         }
     }
 }
